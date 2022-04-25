3892859d15c0382ceb83f3fcc63af4f86a6f50f4
Tom <tomas@indicius.com>
2022-04-25 19:28:34 -0300

    Adds Bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import PrimaryLayout from "layouts/layout-primary"
 
 // Styles
 import "assets/styles/reset.css"
-import "assets/styles/bootstrap.scss"
+import "assets/styles/main.scss"
 
 const MyApp = ({ Component, pageProps }) => (
 	<PrimaryLayout>
