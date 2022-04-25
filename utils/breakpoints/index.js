b3389ee362931379865b5989f305dd718526f76c
Tom <tomas@indicius.com>
2022-04-25 16:33:29 -0300

    Adds utils

---
+++ utils/breakpoints/index.js
@@ -0,0 +1,14 @@
+// Libraries
+import { css } from "styled-components"
+
+// Utils
+import { breakpoints } from "../variables/"
+
+export default Object.keys(breakpoints).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (min-width: ${breakpoints[label]}px) {
+      ${css(...args)};
+    }
+  `
+  return acc
+}, {})
---
+++ utils/variables/index.js
@@ -0,0 +1,26 @@
+// Breakpoints:start
+export const breakpoints = {
+	small: "768",
+	medium: "1024",
+	large: "1200",
+	extraLarge: "1440",
+	wide: "2000",
+}
+// Breakpoints:end
+
+// Colors:start
+export const colors = {
+	// Insert your variables here
+	white: "#FFFFFF",
+	black: "#000",
+}
+// Colors:end
+
+// Font Weights:start
+export const weights = {
+	400: "400",
+	500: "500",
+	600: "600",
+	700: "700",
+}
+// Font Weights:end
