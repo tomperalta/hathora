242f8538b2d62a58124ffad92251fbe68149f88f
Tom <tomas@indicius.com>
2022-04-25 19:19:41 -0300

    Styles / Adds 'Bootstrap'

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
+const path = require("path")
+
 const nextConfig = {
 	reactStrictMode: true,
 	styledComponents: true,
+	sassOptions: {
+		includePaths: [path.join(__dirname, "assets/styles")],
+	},
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 // Layout
 import PrimaryLayout from "layouts/layout-primary"
 
+// Styles
+import "assets/styles/reset.css"
+import "assets/styles/bootstrap.scss"
+
 const MyApp = ({ Component, pageProps }) => (
 	<PrimaryLayout>
 		<Component {...pageProps} />
