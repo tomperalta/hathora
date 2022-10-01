bc2b0690ca3ec0e977725f0ed361607661530342
Juan José Peralta <juanjose@indicius.com>
2022-10-01 13:58:03 -0300

    home / features / listo el issue fixing

--- components/home/features.js
+++ components/home/features.js
@@ -67,7 +67,7 @@ const Features = () => (
 					</h1>
 				</div>
 			</div>
-			<div className="row p-0">
+			<div className="row justify-content-center">
 				<div className="col-12 col-md-5">
 					<div className="feature">
 						<Check className="icon" />
