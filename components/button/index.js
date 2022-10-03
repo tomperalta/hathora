922c3f5c64cf5a00ccd33e72504eb20191713d54
Tom <tomas@indicius.com>
2022-10-03 14:17:14 -0300

    Components / Button / Fixes animation lag issue on Chrome

--- components/button/index.js
+++ components/button/index.js
@@ -31,7 +31,7 @@ export const ButtonStyles = css`
 
 	${(props) =>
 		props.theme === "fill" &&
-		css`
+		`
 			background-color: ${colors.green__500};
 			color: ${colors.grey__700};
 
@@ -47,7 +47,7 @@ export const ButtonStyles = css`
 
 	${(props) =>
 		props.theme === "gradient" &&
-		css`
+		`
 			position: relative;
 			color: ${colors.grey__700};
 			overflow: hidden;
@@ -84,6 +84,7 @@ export const ButtonStyles = css`
 				transform: translate(-50%, -50%);
 				transition: width 0.2s ease, height 0.2s ease, opacity 0.2s ease;
 				opacity: 0;
+        will-change: width, height, opacity;
 				z-index: -1;
 			}
 
@@ -103,7 +104,7 @@ export const ButtonStyles = css`
 
 	${(props) =>
 		props.theme === "outline" &&
-		css`
+		`
 			border: 1px solid ${colors.green__500};
 			color: ${colors.green__500};
 
@@ -120,7 +121,7 @@ export const ButtonStyles = css`
 
   ${(props) =>
 		props.theme === "borderless" &&
-		css`
+		`
 			position: relative;
 			padding: 0;
 			color: ${colors.grey__200};
@@ -192,17 +193,28 @@ const Button = (props) => {
 	const ref = useRef()
 
 	useEffect(() => {
-		if (theme === "gradient" && ref.current) {
-			ref.current.addEventListener("mousemove", (event) => {
-				const { pageX, pageY, target } = event
+		const handleMouseMove = (event) => {
+			const { pageX, pageY, target } = event
 
-				const x = pageX - target.offsetLeft
-				const y = pageY - target.offsetTop
+			const x = pageX - target.offsetLeft
+			const y = pageY - target.offsetTop
 
-				target.style.setProperty("--x", `${x}px`)
-				target.style.setProperty("--y", `${y}px`)
-			})
+			target.style.setProperty("--x", `${x}px`)
+			target.style.setProperty("--y", `${y}px`)
 		}
+
+		document
+			.querySelectorAll('[data-theme="gradient"]')
+			.forEach((button) =>
+				button.addEventListener("mousemove", handleMouseMove, { passive: true })
+			)
+
+		return () =>
+			document
+				.querySelectorAll('[data-theme="gradient"]')
+				.forEach((button) =>
+					button.removeEventListener("mousemove", handleMouseMove)
+				)
 	}, [])
 
 	/**
@@ -213,6 +225,7 @@ const Button = (props) => {
 			<StyledButton
 				ref={ref}
 				theme={theme}
+				data-theme={theme}
 				type="button"
 				className={className}
 				disabled={disabled}
