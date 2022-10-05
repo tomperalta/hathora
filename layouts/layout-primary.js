258ed2bc02d47c5dd28acb036b2d47d9719be6e6
Tom <tomas@indicius.com>
2022-10-04 21:13:51 -0300

    Layout / Adds 'Footer'

--- layouts/layout-primary.js
+++ layouts/layout-primary.js
@@ -8,16 +8,15 @@ import GlobalStyles from "assets/styles/globalStyles"
 
 // Components
 import Menu from "components/menu"
-// import Footer from "components/footer"
 import SignUpModal from "components/sign-up-modal"
-// import Footer from "components/footer"
+import Footer from "components/footer"
 
 const PrimaryLayout = ({ children }) => (
 	<>
 		<GlobalStyles />
 		<Menu />
 		<main>{children}</main>
-		{/* <Footer /> */}
+		<Footer />
 		<SignUpModal />
 	</>
 )
