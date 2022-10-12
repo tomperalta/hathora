361ed0d9c06fd56bd94dac0741eea827f10e8f24
Tom <tomas@indicius.com>
2022-10-12 15:08:27 -0300

    Adds 'Hathora Builder' page

---
+++ pages/hathora-builder.js
@@ -0,0 +1,38 @@
+import React from "react"
+
+// Libraries
+import styled from "styled-components"
+
+// Layout
+import LayoutPrimary from "layouts/layout-primary"
+
+// Utils
+import breakpoint from "utils/breakpoints/"
+
+// Components
+import SEO from "components/seo"
+
+// Sections
+
+const StyledHathoraBuilder = styled.main`
+	> section {
+		padding: 96px 0;
+
+		${breakpoint.medium`
+      padding: 100px 0;
+    `}
+	}
+`
+
+const HathoraBuilder = () => (
+	<StyledHathoraBuilder>
+		<SEO
+			title="Hathora Builder | Serverless Cloud Platform for Multiplayer Games"
+			description="A multiplayer game framework"
+		/>
+	</StyledHathoraBuilder>
+)
+
+export default HathoraBuilder
+
+HathoraBuilder.getLayout = (page) => <LayoutPrimary>{page}</LayoutPrimary>
