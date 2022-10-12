8c0e78be4774abb675e1b828ed3ff99ea6978cd4
Martin Navarro <martinnavarro@Martins-MacBook-Pro.local>
2022-10-12 16:37:44 -0300

    About us / Mission / Completed without mobile

--- components/about-us/mission.js
+++ components/about-us/mission.js
@@ -11,26 +11,31 @@ import Container from "components/container"
 
 // Images
 import { ReactComponent as MissionImage } from "../../assets/images/about-us/ellipse.svg"
+import { ReactComponent as MissionImageSmall } from "../../assets/images/about-us/ellipse-small.svg"
 
 const StyledMission = styled.section`
+	position: relative;
+
 	.image {
-		position: relative;
+		position: absolute;
 		left: 50%;
 		transform: translateX(-50%);
+		top: 0;
 	}
 `
 
 const Mission = () => (
 	<StyledMission>
 		<Container>
-			<div className="row">
-				<div className="col-12 align-items-center justify-content-center">
-					<h2 className="heading--s">
+			<div className="row  justify-content-center align-items-center">
+				<div className="col-12 col-md-10">
+					<h2 className="heading--s text-center">
 						Our team’s mission is to apply our years of experience scaling
 						multi-cloud, multi-region infrastructure to create a platform that
 						suits game developer needs
 					</h2>
-					<MissionImage className="image" />
+					<MissionImage className="image d-none d-md-block" />
+					<MissionImageSmall className="image d-block d-md-none" />
 				</div>
 			</div>
 		</Container>
