52ba6632c7f96cb9f1f908a3ceaa37b89358d0af
Tom <tomas@indicius.com>
2022-10-13 17:33:24 -0300

    Hathora Builder / Game Development / Adds images

--- components/hathora-builder/game-development.js
+++ components/hathora-builder/game-development.js
@@ -5,10 +5,16 @@ import styled from "styled-components"
 
 // Utils
 import breakpoint from "utils/breakpoints/"
+import { colors } from "utils/variables"
 
 // Components
 import Container from "components/container/"
-import { colors } from "utils/variables"
+
+// Icons
+import IconDataPersistence from "assets/icons/hathora-builder/icon-data-persistence.svg"
+import IconStateSync from "assets/icons/hathora-builder/icon-state-sync.svg"
+import IconOptimizedNetworking from "assets/icons/hathora-builder/icon-optimized-networking.svg"
+import Image from "next/image"
 
 const StyledGameDevelopment = styled.section`
 	.game-development__heading {
@@ -62,16 +68,19 @@ const GameDevelopment = () => {
 			title: "Data persistance",
 			description:
 				"Intuitive persistence layer provided out of the box. Update the in-memory game state and the framework handles persistence. ",
+			image: { src: IconDataPersistence, width: 543, height: 300 },
 		},
 		{
 			title: "State sync",
 			description:
 				"Connected clients are always up-to-date. Developers will never have to deal with WebSocket, HTTP or TCP code to get their game running — we take care of that behind the scenes.",
+			image: { src: IconStateSync, width: 543, height: 300 },
 		},
 		{
 			title: "Optimized networking",
 			description:
 				"Declarative data model provided out of the box generates an optimized binary protocol for your data. Combined with powerful delta compression techniques, messages are significantly smaller than JSON snapshots.",
+			image: { src: IconOptimizedNetworking, width: 646, height: 320 },
 		},
 	]
 
@@ -94,6 +103,10 @@ const GameDevelopment = () => {
 								<div className="card__content">
 									<p className="text--l mb-2">{item.title}</p>
 									<p className="text--s">{item.description}</p>
+								</div>
+
+								<div className="card__image">
+									<Image {...item.image} alt="" />
 								</div>
 							</div>
 						</div>
