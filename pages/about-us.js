84c0ba6a23eb2b05c17116aaa8540ca45e407566
Tom <tomas@indicius.com>
2022-10-14 16:36:32 -0300

    About Us / Our Vision / Adds timeline

---
+++ pages/about-us.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+// Libraries
+import styled from "styled-components"
+
+// Layout
+import LayoutPrimary from "layouts/layout-primary"
+
+// Utils
+import breakpoint from "utils/breakpoints/"
+
+// Components
+import SEO from "components/seo"
+import OurVision from "components/about-us/our-vision"
+
+// Sections
+
+const StyledAboutUs = styled.main`
+	> section {
+		padding: 96px 0;
+
+		${breakpoint.medium`
+      padding: 100px 0;
+    `}
+	}
+`
+
+const AboutUs = () => (
+	<StyledAboutUs>
+		<SEO title="About Us | Serverless Cloud Platform for Multiplayer Games" />
+		<OurVision />
+	</StyledAboutUs>
+)
+
+export default AboutUs
+
+AboutUs.getLayout = (page) => <LayoutPrimary>{page}</LayoutPrimary>
