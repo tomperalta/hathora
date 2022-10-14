425dda3f216d11f793486825e3a2ba0d97ef3c62
Tom <tomas@indicius.com>
2022-10-14 08:21:34 -0300

    About us / Hero / Adds arrow animation

--- components/about-us/hero.js
+++ components/about-us/hero.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 // Libraries
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 import Image from "next/image"
 
 // Components
@@ -11,7 +11,25 @@ import Container from "components/container/"
 import breakpoint from "utils/breakpoints/"
 
 // Icons
+import { ReactComponent as IconChevronDown } from "assets/icons/about-us/icon-chevron-down.svg"
 import IconHero from "assets/icons/about-us/icon-hero.svg"
+import { colors } from "utils/variables"
+
+const animation = keyframes`
+  0% {
+		opacity: 0;
+		transform: translateY(-20px);
+	}
+
+	50% {
+		opacity: 1;
+	}
+
+	100% {
+		opacity: 0;
+		transform: translateY(0);
+	}
+`
 
 const StyledHero = styled.section`
 	min-height: 100vh;
@@ -39,30 +57,91 @@ const StyledHero = styled.section`
       right: calc((100vw - 1440px) / 2);
     `}
 	}
+
+	.hero__scroll-down-button {
+		width: 24px;
+		height: 40px;
+		position: absolute;
+		right: 0;
+		bottom: 48px;
+		left: 0;
+		margin: auto;
+
+		svg {
+			position: relative;
+			animation: ${animation} 2s infinite;
+
+			&:nth-child(1) {
+				opacity: 0.3;
+
+				* {
+					fill: ${colors.purple__500};
+				}
+			}
+
+			&:nth-child(2) {
+				top: -20px;
+				opacity: 0.5;
+				animation-delay: -0.2s;
+
+				* {
+					fill: ${colors.green__400};
+				}
+			}
+
+			&:nth-child(3) {
+				top: -40px;
+				animation-delay: -0.4s;
+			}
+		}
+	}
 `
 
-const Hero = () => (
-	<StyledHero className="about__hero">
-		<Container>
-			<div className="row justify-content-center justify-content-md-start align-items-center">
-				<div className="col-12 col-sm-8 col-md-7 p-md-0">
-					<div className="text-sm-center text-md-start">
-						<h2 className="heading--l font-weight--500 mb-4">
-							Hathora is bringing infrastructure expertise to the gaming world
-						</h2>
-					</div>
+const Hero = () => {
+	const handleClick = () => {
+		console.log("Click")
+		const missionSection = document.getElementById("mission")
 
-					<div
-						className="hero__icon"
-						data-aos="zoom-in-up"
-						data-aos-anchor=".about__hero"
-					>
-						<Image src={IconHero} width="608" height="608" alt="" />
+		if (missionSection) {
+			missionSection.scrollIntoView({
+				behavior: "smooth",
+			})
+		}
+	}
+
+	return (
+		<StyledHero className="about__hero">
+			<Container>
+				<div className="row justify-content-center justify-content-md-start align-items-center">
+					<div className="col-12 col-sm-8 col-md-7 p-md-0">
+						<div className="text-sm-center text-md-start">
+							<h2 className="heading--l font-weight--500 mb-4">
+								Hathora is bringing infrastructure expertise to the gaming world
+							</h2>
+						</div>
+
+						<div
+							className="hero__icon"
+							data-aos="zoom-in-up"
+							data-aos-anchor=".about__hero"
+						>
+							<Image src={IconHero} width="608" height="608" alt="" />
+						</div>
 					</div>
 				</div>
-			</div>
-		</Container>
-	</StyledHero>
-)
+
+				<button
+					type="button"
+					className="hero__scroll-down-button"
+					onClick={handleClick}
+				>
+					<IconChevronDown />
+					<IconChevronDown />
+					<IconChevronDown />
+				</button>
+			</Container>
+		</StyledHero>
+	)
+}
 
 export default Hero
