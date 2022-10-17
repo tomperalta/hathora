631e98748ca6acf082f0e0ae7f9779f184869693
Juan José Peralta <juanjose@indicius.com>
2022-10-17 14:34:15 -0300

    home / clients section / WIP

--- components/home/clients.js
+++ components/home/clients.js
@@ -19,11 +19,6 @@ import { ReactComponent as Godot } from "assets/icons/home/clients/godot.svg"
 const StyledClients = styled.section`
 	padding: 24px 0 120px 0;
 
-	${breakpoint.medium`
-    padding: 24px 0 200px 0;
-  
-  `}
-
 	.clients {
 		margin-top: 70px;
 		display: flex;
@@ -43,6 +38,12 @@ const StyledClients = styled.section`
 			${breakpoint.small`
         width: auto;
       `}
+			${breakpoint.medium`
+				max-width: 333px;
+				width: 100%;
+			`}
+
+
 
 			&.client--phaser {
 				${breakpoint.small`
@@ -51,15 +52,15 @@ const StyledClients = styled.section`
 			}
 
 			&.client--hathora {
-				width: 119px;
+				width: 42%;
 				height: 16px;
-				margin-right: 50px;
+				margin-right: 27px;
 
 				${breakpoint.medium`
           width: 215px;
           height: 28px;
-          margin-right: 27px;
-          margin-left: 64px;
+          margin-right: 37px;
+          margin-left: 54px;
         `}
 
 				${breakpoint.large`
@@ -81,58 +82,30 @@ const StyledClients = styled.section`
 `
 
 const Clients = () => (
-	<StyledClients className="home__clients">
+	<StyledClients>
 		<Container>
 			<div className="row justify-content-center">
 				<div className="col-12 col-md-7 text-center">
-					<h1
-						className="heading--m dotted-separator"
-						data-aos="fade-in"
-						data-aos-anchor=".home__clients"
-					>
+					<h1 className="heading--m dotted-separator">
 						Integrated with the game engines you know and love
 					</h1>
 				</div>
 
-				<div className="col-12 col-sm-6 col-md-8 col-lg-7">
+				<div className="col-12 col-sm-6 col-md-8 col-lg-6">
 					<div className="clients">
-						<div
-							className="client client--phaser"
-							data-aos="fade-in"
-							data-aos-delay="250"
-						>
+						<div className="client client--phaser">
 							<Phaser />
 						</div>
-						<div
-							className="client client--unity"
-							data-aos="fade-in"
-							data-aos-anchor="home__clients"
-							data-aos-delay="500"
-						>
+						<div className="client client--unity">
 							<Unity />
 						</div>
-						<div
-							className="client client--hathora"
-							data-aos="fade-in"
-							data-aos-anchor="home__clients"
-							data-aos-delay="750"
-						>
+						<div className="client client--hathora text-center">
 							<Hathora />
 						</div>
-						<div
-							className="client client--godot"
-							data-aos="fade-in"
-							data-aos-anchor="home__clients"
-							data-aos-delay="1000"
-						>
+						<div className="client client--godot text-center">
 							<Godot />
 						</div>
-						<div
-							className="client client--unreal"
-							data-aos="fade-in"
-							data-aos-anchor="home__clients"
-							data-aos-delay="1250"
-						>
+						<div className="client client--unreal">
 							<Unreal />
 						</div>
 					</div>
