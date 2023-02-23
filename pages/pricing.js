8251b11bdab84df788a4c21c4faece28996a2671
Tom <tomas@indicius.com>
2023-02-23 10:57:21 -0300

    Pricing > Adds 'Get in Touch' section

--- pages/pricing.js
+++ pages/pricing.js
@@ -7,12 +7,14 @@ import PrimaryLayout from "layouts/layout-primary"
 import SEO from "components/seo"
 import Plans from "components/pricing/plans"
 import PricingPerMatch from "components/pricing/pricing-per-match"
+import GetInTouch from "components/pricing/get-in-touch"
 
 const Pricing = () => (
 	<PrimaryLayout>
 		<SEO title="Pricing" />
 		<Plans />
 		<PricingPerMatch />
+		<GetInTouch />
 	</PrimaryLayout>
 )
 
