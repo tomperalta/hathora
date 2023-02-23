5e4729350226836b3f6427c584c22f7e3e7d6e67
Tom <tomas@indicius.com>
2023-02-23 15:28:04 -0300

    Pricing > Hathora > Adds 'Discount Banner' section

--- pages/pricing.js
+++ pages/pricing.js
@@ -9,12 +9,14 @@ import Plans from "components/pricing/plans"
 import PricingPerMatch from "components/pricing/pricing-per-match"
 import GetInTouch from "components/pricing/get-in-touch"
 import Features from "components/pricing/features"
+import DiscountBanner from "components/pricing/discount-banner"
 
 const Pricing = () => (
 	<PrimaryLayout>
-		<SEO title="Pricing" />
+		<SEO title="Pricing | Serverless Cloud Platform for Multiplayer Games" />
 		<Plans />
 		<Features />
+		<DiscountBanner />
 		<PricingPerMatch />
 		<GetInTouch />
 	</PrimaryLayout>
