398e112e23358889e867b57da4b7a38b368c9e79
Juan José Peralta <juanjose@indicius.com>
2023-03-09 16:50:03 -0300

    home / testimonials / agregue 3 testimonios nuevos

--- components/home/testimonials.js
+++ components/home/testimonials.js
@@ -113,7 +113,7 @@ const Testimonials = () => {
 		{
 			quote:
 				"The team at Hathora knows what it takes to create high quality infrastructure that scales across the globe, and I'm thrilled they're simplifying the path for more multiplayer games to successfully launch world-wide titles.",
-			author: "Brandi House",
+			author: "Brandi House, General Manager",
 			company: "ProbablyMonsters",
 		},
 		{
