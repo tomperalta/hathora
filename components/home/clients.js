61dcb2eeb908aaadc288c475ccd678d136ffe002
Juan José Peralta <juanjose@indicius.com>
2023-03-12 12:50:44 -0300

    home / clients / any game / arregle la alineacion de las illus

--- components/home/clients.js
+++ components/home/clients.js
@@ -174,7 +174,7 @@ const Clients = () => {
 								</div>
 							</div>
 							<div className="col-12 col-md-4 col-lg-3">
-								<div className="text-center mt-4 mb-4">
+								<div className="text-center mb-4">
 									<p className="text--m color--purple__500">RTS/MOBA</p>
 								</div>
 								<div>
