eb5c6e11f1bbb1ace2c67a4fc89ce1dedd2350ed
Juan José Peralta <juanjose@indicius.com>
2023-03-14 16:25:18 -0300

    testimonials / brandi house / arregle el puesto

--- components/home/testimonials.js
+++ components/home/testimonials.js
@@ -97,8 +97,12 @@ const Testimonials = () => {
 			quote:
 				"The team at Hathora knows what it takes to create high quality infrastructure that scales across the globe, and I'm thrilled they're simplifying the path for more multiplayer games to successfully launch world-wide titles.",
 			author: "Brandi House",
-			company:
-				"General Manager @ ProbablyMonsters, Former Director of Product @ Unity",
+			company: (
+				<>
+					General Manager @ ProbablyMonsters,
+					<br /> Former Director of Product @ Unity
+				</>
+			),
 		},
 		{
 			quote:
