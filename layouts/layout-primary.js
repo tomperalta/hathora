2917dde6208a9bcdba807b2412e17f17b8872f3a
Justin <jchu231@gmail.com>
2023-08-28 10:22:39 -0700

    remove rendeirng of banner for website

--- layouts/layout-primary.js
+++ layouts/layout-primary.js
@@ -11,7 +11,6 @@ import GlobalStyles from "assets/styles/globalStyles"
 import Menu from "components/menu"
 import SignUpModal from "components/sign-up-modal"
 import Footer from "components/footer"
-import FundraiseBanner from "components/fundraise-banner"
 
 const PrimaryLayout = ({ children }) => {
 	useEffect(() => {
@@ -24,7 +23,6 @@ const PrimaryLayout = ({ children }) => {
 	return (
 		<>
 			<GlobalStyles />
-			<FundraiseBanner />
 			<Menu />
 			<main>{children}</main>
 			<Footer />
