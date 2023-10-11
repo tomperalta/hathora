9c3860be2a45c185c0a73a1701ba220867c5a49b
Tom <tomas@indicius.com>
2023-10-11 12:26:57 -0300

    Pricing > Adds FAQs section

--- components/pricing/faqs.js
+++ components/pricing/faqs.js
@@ -9,6 +9,22 @@ const Faqs = () => {
 			content:
 				"You only pay when your games are being played! No idle server costs :).",
 		},
+		{
+			title: "Bandwidth pricing?",
+			content: "I won't accept nothing lower than a million dollars.",
+		},
+		{
+			title: "Here goes another question?",
+			content: (
+				<>
+					Yep. And maybe a <a href="/docs">link</a>?
+				</>
+			),
+		},
+		{
+			title: "Last sample question?",
+			content: "That was all. Well, until there is real content I guess.",
+		},
 	]
 
 	return (
@@ -16,8 +32,10 @@ const Faqs = () => {
 			<Container>
 				<div className="row justify-content-center">
 					<div className="col-12 col-sm-8">
-						{data.map((faq) => (
-							<Accordion title={faq.title}>{faq.content}</Accordion>
+						{data.map((faq, index) => (
+							<Accordion active={index === 0} title={faq.title} key={faq.title}>
+								{faq.content}
+							</Accordion>
 						))}
 					</div>
 				</div>
--- pages/pricing.js
+++ pages/pricing.js
@@ -1,5 +1,11 @@
 import React from "react"
 
+// Libraries
+import styled from "styled-components"
+
+// Utils
+import breakpoints from "utils/breakpoints"
+
 // Components
 import SEO from "components/seo"
 import Plans from "components/pricing/plans"
@@ -7,9 +13,24 @@ import PricingPerMatch from "components/pricing/pricing-per-match"
 import GetInTouch from "components/pricing/get-in-touch"
 import Features from "components/pricing/features"
 import DiscountBanner from "components/pricing/discount-banner"
+import Faqs from "components/pricing/faqs"
+
+const StyledPricing = styled.main`
+	> section {
+		padding: 60px 0;
+
+		&:first-child {
+			padding-top: 120px;
+
+			${breakpoints.medium`
+        padding-top: 148px;
+      `}
+		}
+	}
+`
 
 const Pricing = () => (
-	<>
+	<StyledPricing>
 		<SEO
 			title="Pricing | Serverless Cloud Platform for Multiplayer Games"
 			description="See Hathora’s pricing system that allows you to take your game global, with plans for all game types and bandwidth needs."
@@ -17,9 +38,10 @@ const Pricing = () => (
 		<Plans />
 		<Features />
 		<DiscountBanner />
+		<Faqs />
 		<PricingPerMatch />
 		<GetInTouch />
-	</>
+	</StyledPricing>
 )
 
 export default Pricing
