0d3e18d89b4dcb340ba62969839c0a660f362890
Tom <tomas@indicius.com>
2023-10-16 16:20:52 -0300

    Pricing > FAQs > Adds new copy

--- components/pricing/faqs.js
+++ components/pricing/faqs.js
@@ -5,16 +5,56 @@ import React from "react"
 const Faqs = () => {
 	const data = [
 		{
-			title: "When do I start to pay?",
-			content:
-				"You only pay when your games are being played! No idle server costs :).",
+			title:
+				"What is the difference between pay as you go and commitment pricing on Hathora?",
+			content: (
+				<>
+					<p>
+						To optimize compute costs for our customers, Hathora operates as a
+						hybrid cloud. Hybrid cloud means that we can provision servers on
+						both bare metal and public cloud. We run base (or committed)
+						capacity on bare metal and for unexpected (or burst) capacity we use
+						public cloud.
+					</p>
+
+					<br />
+
+					<p>
+						Hathora’s operating costs are significantly cheaper on bare metal
+						than on public cloud and we pass on the savings to you! However, it
+						does come at the cost of flexibility. We need approximately 2-3
+						weeks to order and set up the servers in our data centers. If you
+						know that your game will require a certain amount of base capacity,
+						we encourage you to reach out for commitment based pricing.
+					</p>
+				</>
+			),
 		},
 		{
-			title: "Bandwidth pricing?",
-			content: "I won't accept nothing lower than a million dollars.",
+			title: "What is a Hathora Compute Unit (HCU)?",
+			content: (
+				<>
+					<p>
+						To optimize compute costs for our customers, Hathora operates as a
+						hybrid cloud. Hybrid cloud means that we can provision servers on
+						both bare metal and public cloud. We run base (or committed)
+						capacity on bare metal and for unexpected (or burst) capacity we use
+						public cloud.
+					</p>
+
+					<p>
+						Hathora’s operating costs are significantly cheaper on bare metal
+						than on public cloud and we pass on the savings to you! However, it
+						does come at the cost of flexibility. We need approximately 2-3
+						weeks to order and set up the servers in our data centers. If you
+						know that your game will require a certain amount of base capacity,
+						we encourage you to reach out for commitment based pricing.
+					</p>
+				</>
+			),
 		},
 		{
-			title: "Here goes another question?",
+			title: "How does the $500 credit work?",
 			content: (
 				<>
 					Yep. And maybe a <a href="/docs">link</a>?
@@ -22,7 +62,8 @@ const Faqs = () => {
 			),
 		},
 		{
-			title: "Last sample question?",
+			title:
+				"What if we already have significant cloud discounts, but don’t want to orchestrate our own servers?",
 			content: "That was all. Well, until there is real content I guess.",
 		},
 	]
