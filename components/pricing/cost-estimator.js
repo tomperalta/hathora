72ca7223cfaee013b15fd651e2aa002e483a775c
Tom <tomas@indicius.com>
2023-10-23 13:37:36 -0300

    Pricing > Cost Estimator > Sets 'Rooms per Process' dropdown value to 1

--- components/pricing/cost-estimator.js
+++ components/pricing/cost-estimator.js
@@ -294,7 +294,7 @@ const CostEstimator = () => {
 			setBandwidth(bandwidth.speed)
 		} else {
 			setvCPU(0.5)
-			setRoomPerProcess(10)
+			setRoomPerProcess(1)
 			setMatchLength(0.25)
 			setBandwidth(1)
 		}
