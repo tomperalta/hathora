5798ace2a0d07ab35a447a98e292e9c82b241084
Justin <jchu231@gmail.com>
2023-12-15 17:03:59 -0800

    add heap.js script

--- pages/_app.js
+++ pages/_app.js
@@ -41,6 +41,39 @@ const MyApp = ({ Component, pageProps }) => (
           gtag('config', 'AW-11183536945');
           gtag('config', 'GTM-PHPK7ZF');`}
 		</Script>
+		<Script strategy="lazyOnload" id="heap">
+			{`(window.heap = window.heap || []),
+			(heap.load = function (e, t) {
+			(window.heap.appid = e), (window.heap.config = t = t || {});
+			var r = document.createElement("script");
+			(r.type = "text/javascript"), (r.async = !0), (r.src = "https://cdn.heapanalytics.com/js/heap-" + e + ".js");
+			var a = document.getElementsByTagName("script")[0];
+			a.parentNode.insertBefore(r, a);
+			for (
+			var n = function (e) {
+			return function () {
+			heap.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+		};
+		},
+			p = [
+			"addEventProperties",
+			"addUserProperties",
+			"clearEventProperties",
+			"identify",
+			"resetIdentity",
+			"removeEventProperty",
+			"setEventProperties",
+			"track",
+			"unsetEventProperty",
+			],
+			o = 0;
+			o < p.length;
+			o++
+			)
+			heap[p[o]] = n(p[o]);
+		});
+			heap.load("959692488");`}
+		</Script>
 		<PrimaryLayout>
 			<Component {...pageProps} />
 		</PrimaryLayout>
