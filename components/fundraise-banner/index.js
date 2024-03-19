709a3db2e6f02f4f4c70063e4824f1d1709101f4
Taruni Paleru <tarunipaleru@gmail.com>
2024-03-19 17:27:25 -0400

    updating text based on Harsh's feedback

--- components/fundraise-banner/index.js
+++ components/fundraise-banner/index.js
@@ -46,7 +46,7 @@ const FundraiseBanner = () => (
 				rel="noopener noreferrer"
 				className="text--s font-weight--700"
 			>
-				Announcing our bare metal server offering
+				Announcing Bare Metal on Hathora
 			</a>
 		</Container>
 	</StyledFundraiseBanner>
