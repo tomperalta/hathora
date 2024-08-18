e3bf85f50e7f285ba888a0aaabdc5a80b234cc58
Justin <jchu231@gmail.com>
2024-08-18 13:32:09 +0200

    cleanup prior to gamescome (remove splitgate ping map, and add logos to homepage

--- layouts/layout-primary.js
+++ layouts/layout-primary.js
@@ -29,7 +29,6 @@ const PrimaryLayout = ({ children }) => {
 		"/mountaintop-studios",
 		"/spectre-divide",
 		"/stormgate",
-		"/splitgate",
 	]
 	const useLayout = !noLayoutPages.includes(router.pathname)
 	return useLayout ? (
--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,7 @@ import Testimonials from "components/home/testimonials"
 // import Deploy from "components/home/deploy"
 import Clients from "components/home/clients"
 import CTACards from "components/home/cta-cards"
+import PoweringLogos from "components/home/powering"
 
 // Icons
 // import { ReactComponent as IconArrow } from "assets/icons/icon-arrow-right.svg"
@@ -54,9 +55,10 @@ const Home = () => (
 	<StyledHome>
 		<SEO />
 		<Hero />
+		<PoweringLogos />
+		<Testimonials />
 		<ModernInfrastructure />
 		{/* <OurPlatform /> */}
-		<Testimonials />
 		{/* <Features /> */}
 		{/* <Deploy /> */}
 		<Clients />
