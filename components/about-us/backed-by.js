8da60e03d0a6377c65cbdd0b1db5ef84d114be71
Robert Guss <robertguss@gmail.com>
2024-11-12 18:12:50 -0500

    style backed by section for mobile

--- components/about-us/backed-by.js
+++ components/about-us/backed-by.js
@@ -5,22 +5,27 @@ import styled from "styled-components"
 import Image from "next/image"
 
 // Layout
-import Container from "components/container"
+import Container from "components/container-new"
+import Line from "components/about-us/divider"
+import { colors } from "utils/variables"
 
 // Images
 import FoundersFund from "../../assets/images/about-us/founders-fund.svg"
 import UpfrontVentures from "../../assets/images/about-us/upfront-ventures.svg"
 import LunarVentures from "../../assets/images/about-us/lunar-ventures.svg"
 
-const StyledBackedBy = styled.section`
-	padding: 32px 0;
-	h2 {
-		margin-bottom: 32px;
-	}
+const StyledH2 = styled.h2`
+	padding: 24px 0 32px 0;
+`
 
-	.image {
-		margin-right: 16px;
-	}
+const StyledCard = styled.div`
+	background: #151521;
+	padding: 32px;
+	border-radius: 10px;
+	color: ${colors.grey__400};
+	font-weight: 700;
+	text-align: center;
+	margin-bottom: 24px;
 `
 
 const cards = [
@@ -39,20 +44,21 @@ const cards = [
 ]
 
 const BackedBy = () => (
-	<StyledBackedBy>
-		<Container>
-			<h2 className="heading--s dotted-separator font-weight--500 text-center">
+	<Container>
+		<div className="row justify-content-center align-items-center mt-24">
+			<Line />
+			<StyledH2 className="heading--m text-center font-weight--500">
 				Backed by
-			</h2>
+			</StyledH2>
 			<div className="row text-center justify-content-center">
 				{cards.map((card) => (
-					<div className="col-12 col-sm-4 col-md-2" key={card.title}>
+					<StyledCard className="col-12 col-sm-4 col-md-2" key={card.title}>
 						<Image src={card.image} alt={card.title} width="180" height="180" />
-					</div>
+					</StyledCard>
 				))}
 			</div>
-		</Container>
-	</StyledBackedBy>
+		</div>
+	</Container>
 )
 
 export default BackedBy
