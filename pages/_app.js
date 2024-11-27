699ec179a650eb544adcb6f685ff318fcca458f2
Robert Guss <robertguss@gmail.com>
2024-11-26 21:17:01 -0500

    add disableTransitionOnChange to theme provider to prevent css flashing and lagging when changing theme

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,7 @@ import "assets/styles/main.scss"
 
 const MyApp = ({ Component, pageProps }) => (
 	<Provider store={store}>
-		<ThemeProvider>
+		<ThemeProvider disableTransitionOnChange>
 			<Script
 				strategy="lazyOnload"
 				src="https://www.googletagmanager.com/gtag/js?id=G-X3F6VCK20S"
