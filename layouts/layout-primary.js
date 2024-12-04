e082fa3befbd7245e498b5aa5c2c7c8fadfc59a7
Robert Guss <robertguss@gmail.com>
2024-12-03 22:21:46 -0500

    scope blog styles and theme changer to only work on /blog route

--- layouts/layout-primary.js
+++ layouts/layout-primary.js
@@ -7,6 +7,7 @@ import AOS from "aos"
 
 // Styles
 import GlobalStyles from "assets/styles/globalStyles"
+import BlogStyles from "assets/styles/blogStyles"
 
 // Components
 import Menu from "components/menu"
@@ -32,9 +33,11 @@ const PrimaryLayout = ({ children }) => {
 		"/splitgate",
 	]
 	const useLayout = !noLayoutPages.includes(router.pathname)
+	const useBlogStyles = router.pathname.includes("/blog")
 	return useLayout ? (
 		<>
-			<GlobalStyles />
+			{!useBlogStyles && <GlobalStyles />}
+			{useBlogStyles && <BlogStyles />}
 			<FundraiseBanner />
 			<Menu />
 			<main>{children}</main>
@@ -43,7 +46,8 @@ const PrimaryLayout = ({ children }) => {
 		</>
 	) : (
 		<>
-			<GlobalStyles />
+			{!useBlogStyles && <GlobalStyles />}
+			{useBlogStyles && <BlogStyles />}
 			<main>{children}</main>
 		</>
 	)
