cbc9dc5e2f5367f4135fd5930a515b6aaedb4734
Robert Guss <robertguss@gmail.com>
2024-12-20 11:05:49 -0500

    remove author page breadcrumbs

--- components/blog/author-hero.js
+++ components/blog/author-hero.js
@@ -1,8 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Link from "next/link"
 import Image from "next/image"
-import { useRouter } from "next/router"
 
 // Components
 import Container from "components/container/"
@@ -40,65 +38,21 @@ const HeroHeading = styled.div`
   `}
 `
 
-const Subtitle = styled.p`
-	color: var(--hero-sub-heading);
-	font-size: 20px;
-	line-height: 28px;
-	font-weight: 400;
-	margin-bottom: 32px;
-`
+const AuthorHero = ({ author }) => (
+	<HeroContainer>
+		<h4>Author</h4>
+		<HeroHeadingContainer>
+			<HeroHeading>{author.name}</HeroHeading>
 
-const RightChevron = () => (
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		width="8"
-		height="13"
-		viewBox="0 0 8 13"
-		fill="none"
-	>
-		<path
-			d="M1 1.5L6 6.5L1 11.5"
-			stroke="#E6E6F2"
-			strokeWidth="2"
-			strokeLinecap="round"
-		/>
-	</svg>
+			<Image
+				src={author.profile_image}
+				alt="Picture of author"
+				width={120}
+				height={120}
+			/>
+		</HeroHeadingContainer>
+	</HeroContainer>
 )
-
-const AuthorHero = ({ author }) => {
-	const router = useRouter()
-	const showBreadcrumb = router.pathname.includes("/blog/author")
-
-	return (
-		<HeroContainer>
-			<h4>Author</h4>
-			<HeroHeadingContainer>
-				<HeroHeading>{author.name}</HeroHeading>
-				<Subtitle>
-					{showBreadcrumb && (
-						<>
-							<Link href="/blog">Blog</Link>
-							<span style={{ margin: "0 24px" }}>
-								<RightChevron />
-							</span>
-							<span>Author</span>
-							<span style={{ margin: "0 24px" }}>
-								<RightChevron />
-							</span>
-							{author.name}
-						</>
-					)}
-				</Subtitle>
-				<Image
-					src={author.profile_image}
-					alt="Picture of author"
-					width={154}
-					height={114}
-				/>
-			</HeroHeadingContainer>
-		</HeroContainer>
-	)
-}
 
 AuthorHero.propTypes = {
 	author: PropTypes.shape({
