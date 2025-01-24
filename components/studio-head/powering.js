beac50d5f08ead8c534b196cbd4fd4fca71fbb43
Robert Guss <robertguss@gmail.com>
2025-01-23 22:13:55 -0500

    style powering section

--- components/studio-head/powering.js
+++ components/studio-head/powering.js
@@ -4,71 +4,80 @@ import Image from "next/image"
 
 const PoweringSection = styled.section`
 	width: 100%;
-	padding: 4rem 0;
-	background: rgba(13, 12, 34, 1);
+	padding: 30px 0 60px;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	gap: 3rem;
+	position: relative;
+
+	.left-margin {
+		margin-left: 92px;
+	}
+`
+
+const Container = styled.div`
+	max-width: 1123px;
+	margin: 0 auto;
 `
 
 const Title = styled.h2`
-	font-size: 2.5rem;
-	color: white;
+	font-size: 48px;
+	font-style: normal;
+	font-weight: 700;
+	line-height: 64px;
+	color: var(--text-primary);
 	text-align: center;
-	margin: 0;
 `
 
 const LogoContainer = styled.div`
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	gap: 4rem;
-	width: 100%;
-	padding: 0 2rem;
+	width: 100%;d
 `
 
 const LogoWrapper = styled.div`
 	height: 60px;
-	display: flex;
-	align-items: center;
 `
 
 const Powering = () => (
 	<PoweringSection>
-		<Title>Powering</Title>
-		<LogoContainer>
-			<LogoWrapper>
-				<Image
-					src="/studio-head/splitgate2_logo.svg"
-					alt="Splitgate 2"
-					width={180}
-					height={60}
-					style={{ objectFit: "contain" }}
-					priority
-				/>
-			</LogoWrapper>
-			<LogoWrapper>
-				<Image
-					src="/studio-head/spectre_logo.png"
-					alt="Spectre"
-					width={180}
-					height={60}
-					style={{ objectFit: "contain" }}
-					priority
-				/>
-			</LogoWrapper>
-			<LogoWrapper>
-				<Image
-					src="/studio-head/stormgate_logo.svg"
-					alt="Stormgate"
-					width={180}
-					height={60}
-					style={{ objectFit: "contain" }}
-					priority
-				/>
-			</LogoWrapper>
-		</LogoContainer>
+		<Container>
+			<Title>Powering</Title>
+			<LogoContainer>
+				<LogoWrapper>
+					<Image
+						src="/studio-head/splitgate2_logo.svg"
+						alt="Splitgate 2"
+						width={180}
+						height={60}
+						style={{ objectFit: "contain" }}
+						priority
+					/>
+				</LogoWrapper>
+				<LogoWrapper className="left-margin">
+					<Image
+						src="/studio-head/spectre_logo.png"
+						alt="Spectre"
+						width={180}
+						height={60}
+						style={{ objectFit: "contain" }}
+						priority
+					/>
+				</LogoWrapper>
+				<LogoWrapper className="left-margin">
+					<Image
+						src="/studio-head/stormgate_logo.svg"
+						alt="Stormgate"
+						width={180}
+						height={60}
+						style={{ objectFit: "contain" }}
+						priority
+					/>
+				</LogoWrapper>
+			</LogoContainer>
+		</Container>
 	</PoweringSection>
 )
 
