d12fba29e8af8bb83aaa78a3d96eca6c8fa9b9f5
Robert Guss <robertguss@gmail.com>
2025-01-23 21:58:33 -0500

    style testimonial author image and info

--- components/studio-head/testimonial.js
+++ components/studio-head/testimonial.js
@@ -4,54 +4,51 @@ import { colors } from "utils/variables"
 
 const TestimonialContainer = styled.div`
 	position: relative;
-	padding: 2rem;
-	background: rgba(13, 12, 34, 0.8);
-	backdrop-filter: blur(10px);
 	width: 100%;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	border: 1px solid red;
 `
 
 const Quote = styled.p`
-	color: ${colors.white};
-	font-size: 1.5rem;
-	line-height: 1.6;
-	text-align: center;
+	color: var(--text-primary);
 	margin-bottom: 2rem;
+	font-size: 32px;
+	font-style: normal;
+	font-weight: 500;
+	line-height: 44px;
 `
 
 const ProfileSection = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	gap: 1rem;
+	margin-top: 57px;
 `
 
 const ProfileImage = styled.img`
-	width: 64px;
-	height: 64px;
-	border-radius: 50%;
+	width: 143px;
+	height: 143px;
+	border-radius: 61.733px;
 	object-fit: cover;
 `
 
 const ProfileInfo = styled.div`
-	display: flex;
-	flex-direction: column;
+	text-align: left;
+	margin-left: 24px;
 `
 
 const Name = styled.h3`
-	color: #8b7fd0;
-	margin: 0;
-	font-size: 1.25rem;
+	color: ${colors.purple__400};
+	font-size: 32px;
+	font-style: normal;
+	font-weight: 700;
+	line-height: 39.509px;
 `
 
 const Title = styled.p`
-	color: ${colors.white};
-	margin: 0;
-	font-size: 1rem;
-	opacity: 0.9;
+	color: ${colors.grey__300};
+	font-size: 23.75px;
+	font-style: normal;
+	font-weight: 400;
+	line-height: 29.632px;
 `
 
 const Testimonial = () => (
@@ -62,7 +59,7 @@ const Testimonial = () => (
 			enabled us to deliver an outstanding experience for players.&rdquo;
 		</Quote>
 		<ProfileSection>
-			<ProfileImage src="/path-to-profile-image.jpg" alt="Nate Mitchell" />
+			<ProfileImage src="/studio-head/nate-mitchell.jpg" alt="Nate Mitchell" />
 			<ProfileInfo>
 				<Name>Nate Mitchell</Name>
 				<Title>Founder & CEO</Title>
