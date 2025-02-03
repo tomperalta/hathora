9340abd0ddde0fe103f9bc5bf1938d20bf42e66d
Robert Guss <robertguss@gmail.com>
2025-02-03 18:08:22 -0500

    create sponsor event at the hub section

---
+++ components/the-hub/sponsor-an-event.js
@@ -0,0 +1,81 @@
+import React from "react"
+import styled from "styled-components"
+import breakpoint from "utils/breakpoints/"
+
+const Container = styled.div`
+	border-radius: 24px;
+	border: 1px solid #2f2f2f;
+	width: 836px;
+	margin: 85px auto 0;
+
+	background: linear-gradient(
+			224deg,
+			rgba(255, 255, 255, 0.09) -2.39%,
+			rgba(0, 0, 0, 0) 75.56%
+		),
+		rgba(2, 254, 87, 0);
+
+	backdrop-filter: blur(42px);
+
+	${breakpoint.medium`
+    padding: 64px 32px;
+  `}
+`
+
+const Content = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	text-align: center;
+`
+
+const Title = styled.h2`
+	color: #fff;
+	text-align: right;
+	font-family: "Space Grotesk";
+	font-size: 32px;
+	font-style: normal;
+	font-weight: 700;
+	line-height: 64px; /* 200% */
+	text-transform: uppercase;
+
+	span {
+		background: linear-gradient(89deg, #a05ef6 83.72%, #65cec3 99.13%);
+		background-clip: text;
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+		font-size: 32px;
+		font-style: normal;
+		font-weight: 700;
+		line-height: 64px;
+		text-transform: uppercase;
+	}
+`
+
+const EmailButton = styled.a`
+	display: flex;
+	padding: 16px 40px;
+	justify-content: center;
+	align-items: center;
+	border-radius: 45px;
+	background: var(--primary-primary-green-500, #02fe57);
+	color: #0e0e1b;
+	font-size: 16px;
+	font-style: normal;
+	font-weight: 700;
+	line-height: 24px;
+`
+
+const SponsorAnEvent = () => (
+	<Container>
+		<Content>
+			<Title>
+				WANT TO SPONSOR AN EVENT AT THE <span>HUB</span>?
+			</Title>
+			<EmailButton href="mailto:contact@hathora.dev">Email us</EmailButton>
+		</Content>
+	</Container>
+)
+
+export default SponsorAnEvent
