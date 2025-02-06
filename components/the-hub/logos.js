98c1de90505b64cafd839b9a0711c65b5ac05b87
Robert Guss <robertguss@gmail.com>
2025-02-06 15:26:27 -0500

    adjust logo sizes

--- components/the-hub/logos.js
+++ components/the-hub/logos.js
@@ -67,8 +67,8 @@ const Logos = () => (
 					<Image
 						src="/the-hub/logos/wildcard.svg"
 						alt="Wildcard"
-						width={130}
-						height={50}
+						width={199}
+						height={63}
 						priority
 					/>
 				</LogoWrapper>
@@ -76,8 +76,8 @@ const Logos = () => (
 					<Image
 						src="/the-hub/logos/mountaintop.svg"
 						alt="Mountaintop"
-						width={130}
-						height={50}
+						width={199}
+						height={63}
 						priority
 					/>
 				</LogoWrapper>
@@ -85,8 +85,8 @@ const Logos = () => (
 					<Image
 						src="/the-hub/logos/naavik.svg"
 						alt="Naavik"
-						width={130}
-						height={50}
+						width={199}
+						height={63}
 						priority
 					/>
 				</LogoWrapper>
@@ -94,52 +94,51 @@ const Logos = () => (
 					<Image
 						src="/the-hub/logos/gambit.svg"
 						alt="Gambit"
-						width={130}
-						height={50}
+						width={199}
+						height={63}
 						priority
 					/>
 				</LogoWrapper>
 
 				{/* Second Row */}
-
 				<LogoWrapper>
 					<Image
 						src="/the-hub/logos/1047.svg"
 						alt="1047"
-						width={130}
-						height={50}
+						width={192}
+						height={61}
 					/>
 				</LogoWrapper>
 				<LogoWrapper>
 					<Image
 						src="/the-hub/logos/i3d.svg"
 						alt="i3D.net"
-						width={130}
-						height={50}
+						width={192}
+						height={61}
 					/>
 				</LogoWrapper>
 				<LogoWrapper>
 					<Image
 						src="/the-hub/logos/nightstreet.svg"
 						alt="Night Street"
-						width={130}
-						height={50}
+						width={192}
+						height={61}
 					/>
 				</LogoWrapper>
 				<LogoWrapper>
 					<Image
 						src="/the-hub/logos/frost-giant.png"
 						alt="Frost Giant"
-						width={130}
-						height={50}
+						width={192}
+						height={61}
 					/>
 				</LogoWrapper>
 				<LogoWrapper>
 					<Image
 						src="/the-hub/logos/Upfront.png"
 						alt="Upfront Ventures"
-						width={130}
-						height={50}
+						width={192}
+						height={61}
 					/>
 				</LogoWrapper>
 
@@ -148,32 +147,32 @@ const Logos = () => (
 					<Image
 						src="/the-hub/logos/Playside.svg"
 						alt="PlaySide"
-						width={130}
-						height={50}
+						width={203}
+						height={65}
 					/>
 				</LogoWrapper>
 				<LogoWrapper>
 					<Image
 						src="/the-hub/logos/GameMakers.svg"
 						alt="GameMakers"
-						width={130}
-						height={50}
+						width={203}
+						height={65}
 					/>
 				</LogoWrapper>
 				<LogoWrapper>
 					<Image
 						src="/the-hub/logos/OMEDA.svg"
 						alt="Omeda Studios"
-						width={130}
-						height={50}
+						width={203}
+						height={65}
 					/>
 				</LogoWrapper>
 				<LogoWrapper>
 					<Image
 						src="/the-hub/logos/FLOREN.svg"
 						alt="Floren Tech"
-						width={130}
-						height={50}
+						width={203}
+						height={65}
 					/>
 				</LogoWrapper>
 
@@ -182,40 +181,40 @@ const Logos = () => (
 					<Image
 						src="/the-hub/logos/Coreloop.svg"
 						alt="Core Loop"
-						width={130}
-						height={50}
+						width={178}
+						height={57}
 					/>
 				</LogoWrapper>
 				<LogoWrapper>
 					<Image
 						src="/the-hub/logos/pragma.png"
 						alt="Pragma"
-						width={130}
-						height={50}
+						width={178}
+						height={57}
 					/>
 				</LogoWrapper>
 				<LogoWrapper>
 					<Image
 						src="/the-hub/logos/A16Z.svg"
 						alt="A167 Games"
-						width={130}
-						height={50}
+						width={178}
+						height={57}
 					/>
 				</LogoWrapper>
 				<LogoWrapper>
 					<Image
 						src="/the-hub/logos/OMDIA.svg"
 						alt="Omdia"
-						width={130}
-						height={50}
+						width={178}
+						height={51}
 					/>
 				</LogoWrapper>
 				<LogoWrapper>
 					<Image
 						src="/the-hub/logos/invokation.svg"
 						alt="Invokation Games"
-						width={130}
-						height={50}
+						width={178}
+						height={57}
 					/>
 				</LogoWrapper>
 			</LogoGrid>
